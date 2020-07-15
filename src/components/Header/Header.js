2d354447479ceb2ff565c52d9fa03bc2545ae319
Luke Buckheit <luke.buckheit@tickpick.com>
2020-07-14 23:40:50 -0400

    almost donw

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './Header.css';
 
-const Header = () => (
-  <div className="Header">
-    <img id="starsBg" src={`${process.env.PUBLIC_URL}/stars.png`} />
-    <div id="breadWordmark">
-        <img id="breadHero" src={`${process.env.PUBLIC_URL}/breadTag.png`} />
-        <img id="animatedWordmarkHero" src={`${process.env.PUBLIC_URL}/animatedWordmark.png`} />
-    </div>
-    {/* TODO - USE THE ACTUAL ASTERISK ASSET */}
-    <button id="toLatestDesktop" className="desktopOnly">GET LATEST VOLUME</button>
-    <button id="toLatestMobile" className="mobileOnly hoverRed">GET VOLUME 1: BREAD</button>
-  </div>
-);
+const Header = () => {
+    function scrollToIssue(type) {
+        const offset = type === 'd' ? 0 : 54;
+        window.scrollTo({ top: window.innerHeight - offset, behavior: 'smooth' });
+    }
+
+    return (
+      <div className="Header">
+        <img id="starsBg" src={`${process.env.PUBLIC_URL}/stars.png`} />
+        <div id="breadWordmark">
+            <img id="breadHero" src={`${process.env.PUBLIC_URL}/breadTag.png`} />
+            <img id="animatedWordmarkHero" src={`${process.env.PUBLIC_URL}/animatedWordmark.png`} />
+        </div>
+        <button id="toLatestDesktop" className="desktopOnly" onClick={() => scrollToIssue('d')}>GET LATEST VOLUME</button>
+        <button id="toLatestMobile" className="mobileOnly hoverRed" onClick={() => scrollToIssue('m')}>GET VOLUME 1: BREAD</button>
+      </div>
+    )
+};
 
 Header.propTypes = {};
 
