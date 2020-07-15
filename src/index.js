799287386e76fcacd73761ece3baeab301557956
Luke Buckheit <luke.buckheit@tickpick.com>
2020-07-14 20:43:54 -0400

    fonts

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './fonts/MonumentExtended.otf';
+import './fonts/AndaleMono.ttf';
+import './fonts/MonumentGrotesk.otf';
+import './fonts/MonumentGroteskMono.otf';
 
 ReactDOM.render(
   <React.StrictMode>
