dff5ef82569c2e45e313c56ec8ba416ad34893d9
Luke Buckheit <luke.buckheit@tickpick.com>
2020-07-15 00:21:11 -0400

    done

--- src/components/Issue/Issue.js
+++ src/components/Issue/Issue.js
@@ -30,7 +30,8 @@ const Issue = () => (
     </div>
     <div className="issueTools">
         <div className="toolsHeader">
-            <span>TOOLS FOR VOLUME 01</span>
+            <span className="desktopOnly">TOOLS FOR VOL. 01</span>
+            <span className="mobileOnly">TOOLS FOR VOLUME 01</span>
         </div>
         <Tool title="10 Second Consulting" imgSrc="shh.png"></Tool>
         <Tool title="MPFREE" imgSrc="mpfree.png"></Tool>
